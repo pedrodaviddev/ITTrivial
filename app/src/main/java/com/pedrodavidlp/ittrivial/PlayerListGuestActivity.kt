62e7b9e4820d7a6c75a6011467fc024a3e4e4943
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2017-05-02 10:49:09 +0200

    PlayerListGuestActivity recyclerview adapter created and tested with example

--- app/src/main/java/com/pedrodavidlp/ittrivial/PlayerListGuestActivity.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/PlayerListGuestActivity.kt
@@ -1,7 +1,8 @@
 package com.pedrodavidlp.ittrivial
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_player_list_guest.*
 
 class PlayerListGuestActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class PlayerListGuestActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_player_list_guest)
+    guestPlayerList.adapter = PlayerListAdapter()
+    guestPlayerList.layoutManager = LinearLayoutManager(applicationContext)
   }
 }
