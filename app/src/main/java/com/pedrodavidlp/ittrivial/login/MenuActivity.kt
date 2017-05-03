54a08998c54e354684192c733f9cf541077891a4
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2017-05-03 12:57:12 +0200

    Created menu view and implemented base navigation

--- app/src/main/java/com/pedrodavidlp/ittrivial/login/MenuActivity.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/login/MenuActivity.kt
@@ -3,11 +3,24 @@ package com.pedrodavidlp.ittrivial.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.pedrodavidlp.ittrivial.R
+import com.pedrodavidlp.ittrivial.match.PlayerListAdminActivity
+import com.pedrodavidlp.ittrivial.match.PlayerListGuestActivity
+import kotlinx.android.synthetic.main.activity_menu.*
+import org.jetbrains.anko.startActivity
 
 class MenuActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_menu)
+    createMatchButton.setOnClickListener {
+      startActivity<PlayerListAdminActivity>()
+      finish()
+    }
+    joinMatchButton.setOnClickListener {
+      startActivity<PlayerListGuestActivity>()
+      finish()
+    }
+
   }
 }
--- app/src/main/java/com/pedrodavidlp/ittrivial/match/PlayerListAdminActivity.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/match/PlayerListAdminActivity.kt
@@ -1,11 +1,12 @@
 package com.pedrodavidlp.ittrivial.match
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-
 import com.pedrodavidlp.ittrivial.R
+import com.pedrodavidlp.ittrivial.game.MatchActivity
 import kotlinx.android.synthetic.main.activity_player_list_admin.*
+import org.jetbrains.anko.startActivity
 
 class PlayerListAdminActivity : AppCompatActivity() {
 
@@ -14,5 +15,8 @@ class PlayerListAdminActivity : AppCompatActivity() {
     setContentView(R.layout.activity_player_list_admin)
     adminPlayerList.adapter = PlayerListAdapter()
     adminPlayerList.layoutManager = LinearLayoutManager(applicationContext)
+    startGameButton.setOnClickListener {
+      startActivity<MatchActivity>()
+    }
   }
 }
