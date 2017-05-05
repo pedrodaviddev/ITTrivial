aaee536487e54a5553d48e008d2d5b8e14f65c99
frnlpz <fraloag@inf.upv.es>
2017-05-05 13:11:12 +0200

    Fixed firebase support to get questions for Enterprise category

--- app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
@@ -2,6 +2,7 @@ package com.pedrodavidlp.ittrivial.game.data
 
 import com.google.firebase.database.*
 import com.pedrodavidlp.ittrivial.game.contract.QuestionContract
+import com.pedrodavidlp.ittrivial.game.domain.Question
 import com.pedrodavidlp.ittrivial.game.domain.QuestionRepository
 
 class FireQuestionRepository: QuestionRepository {
@@ -11,13 +12,16 @@ class FireQuestionRepository: QuestionRepository {
   override fun loadQuestion(callback: QuestionContract.InteractorOutput) {
     ref.addValueEventListener(object: ValueEventListener{
       override fun onDataChange(dataSnapShot: DataSnapshot) {
-        var q1: DataSnapshot = dataSnapShot.child("enterprise").child("q1").getValue(String::class.java)
+        var q1: DataSnapshot = dataSnapShot.child("enterprise").child("q1")
 
         var question: String = q1.child("question").getValue(String::class.java)
         var answer: String   = q1.child("answer").getValue(String::class.java)
         var wAns1: String    = q1.child("wans1").getValue(String::class.java)
         var wAns2: String    = q1.child("wans2").getValue(String::class.java)
         var wAns3: String    = q1.child("wans3").getValue(String::class.java)
+
+        val res = Question(question,answer,wAns1,wAns2,wAns3)
+        callback.onQuestionLoaded(res)
       }
 
       override fun onCancelled(error: DatabaseError?) {
