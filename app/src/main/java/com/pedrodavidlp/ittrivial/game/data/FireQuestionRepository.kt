7f5f597d897208e809d2c457e37ac91aa4a24354
frnlpz <fraloag@inf.upv.es>
2017-05-05 13:08:17 +0200

    Added firebase support to get questions for Enterprise category

--- app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
@@ -1,4 +1,31 @@
 package com.pedrodavidlp.ittrivial.game.data
 
-class FireQuestionRepository {
+import com.google.firebase.database.*
+import com.pedrodavidlp.ittrivial.game.contract.QuestionContract
+import com.pedrodavidlp.ittrivial.game.domain.QuestionRepository
+
+class FireQuestionRepository: QuestionRepository {
+  val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+  val ref: DatabaseReference = database.getReference("questions")
+
+  override fun loadQuestion(callback: QuestionContract.InteractorOutput) {
+    ref.addValueEventListener(object: ValueEventListener{
+      override fun onDataChange(dataSnapShot: DataSnapshot) {
+        var q1: DataSnapshot = dataSnapShot.child("enterprise").child("q1").getValue(String::class.java)
+
+        var question: String = q1.child("question").getValue(String::class.java)
+        var answer: String   = q1.child("answer").getValue(String::class.java)
+        var wAns1: String    = q1.child("wans1").getValue(String::class.java)
+        var wAns2: String    = q1.child("wans2").getValue(String::class.java)
+        var wAns3: String    = q1.child("wans3").getValue(String::class.java)
+      }
+
+      override fun onCancelled(error: DatabaseError?) {
+        callback.onError()
+      }
+
+    })
+  }
+
+
 }
