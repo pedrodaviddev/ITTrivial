34b69269975ae301af4d69dccf6ceba1370f90aa
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2017-05-10 23:42:40 +0200

    Apply observer in MockLobbyRepository

--- app/src/main/java/com/pedrodavidlp/ittrivial/login/data/MockLobbyRepository.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/login/data/MockLobbyRepository.kt
@@ -2,19 +2,26 @@ package com.pedrodavidlp.ittrivial.login.data
 
 import android.os.Handler
 import android.os.Looper
+import com.pedrodavidlp.ittrivial.base.Observer
 import com.pedrodavidlp.ittrivial.game.domain.model.Game
 import com.pedrodavidlp.ittrivial.login.contract.GameListContract
 import com.pedrodavidlp.ittrivial.login.contract.UserListContract
 import com.pedrodavidlp.ittrivial.login.domain.model.User
 import com.pedrodavidlp.ittrivial.login.domain.repository.LobbyRepository
+import kotlin.properties.Delegates
 
 class MockLobbyRepository: LobbyRepository {
-  override fun getGames(callback: GameListContract.InteractorOutput) {
-    callback.onFetchGameListSuccess(listOf(Game("Solo hay una partida")))
+  override fun getGames(observer: Observer<List<Game>>): List<Game> {
+    var listGames: List<Game> by Delegates.observable(emptyList()) { _, _, new ->
+     observer.onChange(new)
+    }
     Handler(Looper.getMainLooper()).postDelayed({
-      callback.onFetchGameListSuccess(listOf(Game("Eyy compañeras"),
-          Game("Hola prhema"), Game("Como estais locos")))
-    }, 15000)
+      listGames = listOf(Game("Franlo gay"))
+    },500)
+    Handler(Looper.getMainLooper()).postDelayed({
+      listGames = listOf(Game("Esto no va a ir"),Game("Esto no va a ir"),Game("Esto no va a ir"))
+    },1000)
+    return listGames
   }
 
   override fun getUsersInGame(game: Game, callback: UserListContract.InteractorOutput) {
--- app/src/main/java/com/pedrodavidlp/ittrivial/login/domain/usecase/GetGameList.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/login/domain/usecase/GetGameList.kt
@@ -1,14 +1,20 @@
 package com.pedrodavidlp.ittrivial.login.domain.usecase
 
+import com.pedrodavidlp.ittrivial.base.Observer
 import com.pedrodavidlp.ittrivial.game.domain.model.Game
 import com.pedrodavidlp.ittrivial.login.contract.GameListContract
 import com.pedrodavidlp.ittrivial.login.domain.repository.LobbyRepository
 import kotlin.concurrent.thread
 
-class GetGameList(val repository: LobbyRepository): GameListContract.Interactor {
+class GetGameList(val repository: LobbyRepository) : GameListContract.Interactor {
   override fun getGameList(callback: GameListContract.InteractorOutput) {
     thread {
-      repository.getGames(callback)
+      repository.getGames(object : Observer<List<Game>> {
+        override fun onChange(newValue: List<Game>) {
+          callback.onFetchGameListSuccess(newValue)
+        }
+      })
+
     }
   }
 }
