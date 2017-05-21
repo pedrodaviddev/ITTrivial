41f4672b4166d21e40464b89a745c1b350ba31ad
frnlpz <fraloag@inf.upv.es>
2017-05-21 12:15:10 +0200

    Added firebase behavior to Question repository

--- app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/game/data/FireQuestionRepository.kt
@@ -2,38 +2,33 @@ package com.pedrodavidlp.ittrivial.game.data
 
 import com.google.firebase.database.*
 import com.pedrodavidlp.ittrivial.game.contract.QuestionContract
+import com.pedrodavidlp.ittrivial.game.domain.model.Game
 import com.pedrodavidlp.ittrivial.game.domain.model.Player
 import com.pedrodavidlp.ittrivial.game.domain.model.Question
 import com.pedrodavidlp.ittrivial.game.domain.repository.QuestionRepository
+import java.util.*
 
 class FireQuestionRepository: QuestionRepository {
-  val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+  val firebase: FirebaseDatabase = FirebaseDatabase.getInstance()
+  val ref : DatabaseReference = firebase.reference
 
-  val ref: DatabaseReference = database.getReference("questions")
-  override fun getQuestion(callback: QuestionContract.InteractorOutput) {
-    ref.addValueEventListener(object: ValueEventListener{
-      override fun onDataChange(dataSnapShot: DataSnapshot) {
-        var q1: DataSnapshot = dataSnapShot.child("enterprise").child("q1")
-
-        var question: String = q1.child("question").getValue(String::class.java)
-        var answer: String   = q1.child("answer").getValue(String::class.java)
-        var wAns1: String    = q1.child("wans1").getValue(String::class.java)
-        var wAns2: String    = q1.child("wans2").getValue(String::class.java)
-        var wAns3: String    = q1.child("wans3").getValue(String::class.java)
-
-        val res = Question(question,answer,wAns1,wAns2,wAns3)
-        callback.onQuestionLoaded(res)
-      }
-
-      override fun onCancelled(error: DatabaseError?) {
+  override fun getQuestion(category: String, callback: QuestionContract.InteractorOutput) {  // To change: category is a Category, not a String.
+    ref.child("questions").child(category).addValueEventListener(object : ValueEventListener{
+      override fun onCancelled(p0: DatabaseError?) {
         callback.onError()
       }
 
+      override fun onDataChange(dataSnapshot: DataSnapshot) {
+        val random = Random()
+        val numQuestions = dataSnapshot.childrenCount.toInt()
+        val num = random.nextInt(numQuestions - 1)+1
+        val question = dataSnapshot.child("q$num").value
+        callback.onQuestionLoaded(question as Question)
+      }
     })
   }
 
-  override fun updateCrowns(player: Player) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  override fun updateCrowns(game: Game, category: String, player: Player) { //Same as before
+    ref.child("games").child(game.name).child("players").child(player.username).child(category).setValue(1)
   }
-
 }
