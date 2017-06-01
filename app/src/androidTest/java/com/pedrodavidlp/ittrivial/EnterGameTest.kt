483c1342e0b2c73ca86b79dc77a814e704b7e00f
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2017-06-01 11:36:33 +0200

    Added First UITest

---
+++ app/src/androidTest/java/com/pedrodavidlp/ittrivial/EnterGameTest.kt
@@ -0,0 +1,49 @@
+package com.pedrodavidlp.ittrivial
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.pedrodavidlp.ittrivial.login.domain.usecase.EnterGame
+import com.pedrodavidlp.ittrivial.login.view.EnterGameActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import android.support.test.InstrumentationRegistry.getTargetContext
+import android.content.ComponentName
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+
+
+@RunWith(AndroidJUnit4::class)
+class EnterGameTest {
+
+  val USERNAME_TO_BE_TYPED = "Conrado"
+
+  @Rule @JvmField
+  val activity = ActivityTestRule<EnterGameActivity>(EnterGameActivity::class.java)
+
+
+  @Test
+  fun testWhenTypeUserAndEnterGame() {
+    onView(withId(R.id.usernameInput))
+        .perform(typeText(USERNAME_TO_BE_TYPED), closeSoftKeyboard())
+    onView(withId(R.id.playButton)).perform(click())
+
+    onView(withId(R.id.welcomeText)).check(matches(withText("Bienvenido $USERNAME_TO_BE_TYPED")))
+  }
+
+  @Test
+  fun testWhenTextFieldIsEmptyAndEnterGame() {
+    onView(withId(R.id.playButton)).perform(click())
+
+    //Check if an activity is Finishing
+    assertFalse(activity.getActivity().isFinishing());
+  }
+
+}
+
