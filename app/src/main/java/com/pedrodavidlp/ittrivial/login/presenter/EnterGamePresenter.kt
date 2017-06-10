598bcab7e21b055555d43775621ef7b836c107bf
frnlpz <fraloag@inf.upv.es>
2017-06-10 11:24:08 +0200

    Matcher for special characters in username. usernameError function TO-DO

--- app/src/main/java/com/pedrodavidlp/ittrivial/login/presenter/EnterGamePresenter.kt
+++ app/src/main/java/com/pedrodavidlp/ittrivial/login/presenter/EnterGamePresenter.kt
@@ -1,9 +1,20 @@
 package com.pedrodavidlp.ittrivial.login.presenter
 
 import com.pedrodavidlp.ittrivial.base.domain.data.Session
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class EnterGamePresenter {
   fun pickUsername(username: String){
-    Session.username = username
+    val pattern = Pattern.compile("[^A-Za-z0-9]")
+    val match = pattern.matcher(username)
+    val ok = match.find() && username.isNotEmpty()
+    if(ok)
+      Session.username = username
+    else usernameError()
+  }
+
+  fun usernameError(){
+    //TO-DO
   }
 }
